e43363cd705349dd55c5ff52b03fa4a4b446a5fc
Andreas Holstenson <a@holstenson.se>
2026-05-14 09:36:07 +0200

    feat: Add library for Windows

--- libs.go
+++ libs.go
@@ -4,4 +4,5 @@ package loro
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-apple-darwin -lloro
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-unknown-linux-musl -lloro
 // #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-unknown-linux-musl -lloro
+// #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-pc-windows-gnu -lloro -lkernel32 -lbcrypt -lsynchronization
 import "C"
