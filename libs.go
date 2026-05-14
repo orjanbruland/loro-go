ec3f6eeda25043710ea1d7ff852c53222ae98e80
Andreas Holstenson <a@holstenson.se>
2026-05-14 09:52:55 +0200

    build: Add missing Windows system libraries to LDFLAGS

--- libs.go
+++ libs.go
@@ -4,5 +4,5 @@ package loro
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-apple-darwin -lloro
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-unknown-linux-musl -lloro
 // #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-unknown-linux-musl -lloro
-// #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-pc-windows-gnu -lloro -lkernel32 -lbcrypt -lsynchronization
+// #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-pc-windows-gnu -lloro -lbcrypt -ladvapi32 -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
 import "C"
